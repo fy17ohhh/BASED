47a2c700e93c5849ef279c3c758f1de314f5fc02
fy17ohhh <hester03sfy@gmail.com>
2024-11-20 23:22:50 +0800

    Use 5-star for users to better interpret metric values

--- based-frontend/src/components/StatisticsMetrics.vue
+++ based-frontend/src/components/StatisticsMetrics.vue
@@ -1,32 +1,58 @@
 <template>
   <div class="statistics-metrics-container">
-    <div v-if="groupedData" v-for="(items, categoryDisplay) in groupedData" :key="categoryDisplay" class="metric-item">
+    <div
+      v-if="groupedData"
+      v-for="(items, categoryDisplay) in groupedData"
+      :key="categoryDisplay"
+      class="metric-item"
+    >
       <h3 class="category-title">{{ categoryDisplay }}</h3>
 
       <div v-for="item in items" :key="item.id" class="metric-details">
         <div class="metric-row">
-          <div class="metric-value-left"
-               :class="item.metricName.toLowerCase().replace(/_/g, '-').concat('-value')">
+          <div
+            class="metric-value-left"
+            :class="
+              item.metricName.toLowerCase().replace(/_/g, '-').concat('-value')
+            "
+          >
             {{ item.metricValue }}
           </div>
 
           <div class="metric-bar-container">
             <div
-                class="metric-bar"
-                :class="item.metricName.toLowerCase().replace(/_/g, '-')"
-                :style="{ width: calculatePercentage(item.metricName, item.metricValue) + '%' }"
+              class="metric-bar"
+              :class="item.metricName.toLowerCase().replace(/_/g, '-')"
+              :style="{
+                width:
+                  calculatePercentage(item.metricName, item.metricValue) + '%',
+              }"
             ></div>
             <div class="metric-name">
               {{ item.metricNameDisplay }}
-              <el-tooltip v-if="metricDetails[item.metricName]" :content="`${metricDetails[item.metricName]?.metricDescription}`" placement="bottom" @click.stop.prevent>
+              <el-tooltip
+                v-if="metricDetails[item.metricName]"
+                :content="`${
+                  metricDetails[item.metricName]?.metricDescription
+                }`"
+                placement="bottom"
+                @click.stop.prevent
+              >
                 <i class="el-icon-question" />
               </el-tooltip>
             </div>
           </div>
 
           <div class="metric-percentage">
-            {{ calculatePercentage(item.metricName, item.metricValue) }}%
-            <el-tooltip v-if="metricDetails[item.metricName]" :content="`${metricDetails[item.metricName]?.metricRelationToBias}`" placement="bottom" @click.stop.prevent>
+            {{ calculateStar(item.metricName, item.metricValue) }}
+            <el-tooltip
+              v-if="metricDetails[item.metricName]"
+              :content="`${
+                metricDetails[item.metricName]?.metricRelationToBias
+              }`"
+              placement="bottom"
+              @click.stop.prevent
+            >
               <i class="el-icon-question" />
             </el-tooltip>
           </div>
@@ -37,15 +63,15 @@
 </template>
 
 <script setup>
-import { defineProps, ref, watch } from 'vue';
-import axios from 'axios';
+import { defineProps, ref, watch } from "vue";
+import axios from "axios";
 
 const props = defineProps({
   statisticsMetrics: {
     type: Object,
     required: true,
-    default: () => ({ data: [] })
-  }
+    default: () => ({ data: [] }),
+  },
 });
 
 const groupedData = ref({});
@@ -53,7 +79,7 @@ const metricDetails = ref({});
 
 const processStatisticsMetricsData = async () => {
   if (props.statisticsMetrics) {
-    props.statisticsMetrics.forEach(item => {
+    props.statisticsMetrics.forEach((item) => {
       if (!groupedData.value[item.biasCategoryDisplay]) {
         groupedData.value[item.biasCategoryDisplay] = [];
       }
@@ -61,20 +87,27 @@ const processStatisticsMetricsData = async () => {
     });
 
     // Fetch metric details for each metric
-    const metricNames = [...new Set(props.statisticsMetrics.map(item => item.metricName))];
+    const metricNames = [
+      ...new Set(props.statisticsMetrics.map((item) => item.metricName)),
+    ];
     for (const metricName of metricNames) {
       try {
-        const response = await axios.get(`http://localhost:8081/api/dataset/metric/details?metricName=${metricName}`);
+        const response = await axios.get(
+          `http://localhost:8081/api/dataset/metric/details?metricName=${metricName}`
+        );
         metricDetails.value[metricName] = response.data.data;
       } catch (error) {
-        console.error(`Error fetching details for metric ${metricName}:`, error);
+        console.error(
+          `Error fetching details for metric ${metricName}:`,
+          error
+        );
       }
     }
 
     console.log("Grouped Data based on bias category: ", groupedData.value);
     console.log("Metric Details: ", metricDetails.value);
   } else {
-    console.error('Statistics Metrics Data is null or undefined');
+    console.error("Statistics Metrics Data is null or undefined");
   }
 };
 
@@ -84,10 +117,64 @@ const calculatePercentage = (metricName, metricValue) => {
   return percentage.toFixed(0);
 };
 
-watch(() => props.statisticsMetrics, () => {
-  groupedData.value = {};
-  processStatisticsMetricsData();
-}, { immediate: true });
+const calculateStar = (metricName, metricValue) => {
+  let star = "";
+  switch (metricName) {
+    case "normalized_standard_deviation":
+      star = calculateStarRating1(metricValue);
+      break;
+    case "inverse_imbalance_ratio":
+      star = calculateStarRating2(metricValue);
+      break;
+    case "simpsons_index":
+      star = calculateStarRating1(metricValue);
+      break;
+    default:
+      star = "N/A";
+  }
+  return star;
+};
+
+const calculateStarRating1 = (metricValue) => {
+  let star = "";
+  if (metricValue >= 0.8) {
+    star = "5-star (high bias)";
+  } else if (metricValue >= 0.6) {
+    star = "4-star (moderate bias)";
+  } else if (metricValue >= 0.4) {
+    star = "3-star (neutral)";
+  } else if (metricValue >= 0.2) {
+    star = "2-star (low bias)";
+  } else {
+    star = "1-star (rare bias)";
+  }
+  return star;
+};
+
+const calculateStarRating2 = (metricValue) => {
+  let star = "";
+  if (metricValue <= 0.2) {
+    star = "5-star (high bias)";
+  } else if (metricValue <= 0.4) {
+    star = "4-star (moderate bias)";
+  } else if (metricValue <= 0.6) {
+    star = "3-star (neutral)";
+  } else if (metricValue <= 0.8) {
+    star = "2-star (low bias)";
+  } else {
+    star = "1-star (rare bias)";
+  }
+  return star;
+};
+
+watch(
+  () => props.statisticsMetrics,
+  () => {
+    groupedData.value = {};
+    processStatisticsMetricsData();
+  },
+  { immediate: true }
+);
 </script>
 
 <style scoped>
@@ -146,7 +233,7 @@ watch(() => props.statisticsMetrics, () => {
   background-color: #ff7043;
 }
 
-.normalized-standard-deviation{
+.normalized-standard-deviation {
   background-color: #42a5f5;
 }
 
@@ -173,6 +260,6 @@ watch(() => props.statisticsMetrics, () => {
   font-size: 18px;
   font-weight: 700;
   color: #9e9e9e;
-  width: 50px;
+  width: 200px;
 }
 </style>
